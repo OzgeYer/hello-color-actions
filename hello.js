96719bf3a05187a677999d482a70f3fe25c652e4
OzgeYer <ozge.yerlikaya1@gmail.com>
2025-08-24 23:26:51 +0200

    Update hello.js string value

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from Leiden, it is a beautiful city!";
 console.log(hello());
 exports.hello = hello;
